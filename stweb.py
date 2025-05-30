229a524db2dd35563dc619e837d44b64927c3f0f
harihararaja <harihararajan30@gmail.com>
2025-05-30 16:02:24 +0530

    Update stweb.py

--- stweb.py
+++ stweb.py
@@ -45,9 +45,9 @@ if st.checkbox(F"I HAVE FULL THE INFORMATION"):
         st.error("CHECK THE MOTHRE NAME")
     if not sta:
         st.error("ENTER THE STATE")
-    if len(Fnums) != 10: 
+    if not Fnums or len(Fnums) != 10: 
         st.error("CHECK THE FATHER NUMBER .")
-    if len(Mnums) != 10:
+    if not Mnums or len(Mnums) != 10:
         st.error("CHECK THE MOTHER NUMBER.")
 
 st.button("CANCEL")
